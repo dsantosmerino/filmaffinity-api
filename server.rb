6faadf8de0094ee88514bbedf684b41bfadabc6e
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-27 10:29:40 +0200

    remove imgur_id param validation

--- server.rb
+++ server.rb
@@ -5,7 +5,6 @@ before "/api/*" do
   content_type "application/json"
   lang = params[:lang]
   imgur_id = params[:imgur_id]
-  imgur_id = nil if imgur_id == "nil"
   FilmAffinity.configure do |config|
     config.language = lang
     config.imgur_id = imgur_id
